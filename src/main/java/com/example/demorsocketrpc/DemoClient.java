0493951cff4d74f6f0b141ee8172e92fa9f52116
Toshiaki Maki <tmaki@pivotal.io>
2018-11-07 02:32:42 +0900

    use block() to get a client instance

--- src/main/java/com/example/demorsocketrpc/DemoClient.java
+++ src/main/java/com/example/demorsocketrpc/DemoClient.java
@@ -3,28 +3,26 @@ package com.example.demorsocketrpc;
 import io.rsocket.RSocketFactory;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
 
 public class DemoClient {
+
     public static void main(String[] args) throws Exception {
-        Mono<SimpleServiceClient> clientMono = RSocketFactory.connect()
-                .transport(TcpClientTransport.create(8801))
-                .start()
-                .map(SimpleServiceClient::new);
+        SimpleServiceClient client = RSocketFactory.connect()
+            .transport(TcpClientTransport.create(8801))
+            .start()
+            .map(SimpleServiceClient::new)
+            .block();
 
         CountDownLatch latch = new CountDownLatch(1);
-        Flux<SimpleResponse> response = clientMono
-                .flatMapMany(client -> {
-                    Flux<SimpleRequest> request = Flux.interval(Duration.ofSeconds(1))
-                            .take(10)
-                            .map(i -> SimpleRequest.newBuilder().setRequestMessage("hi" + i).build());
-                    return client.streamingRequestAndResponse(request);
-                })
-                .log()
-                .doFinally(x -> latch.countDown());
+        Flux<SimpleRequest> request = Flux.interval(Duration.ofSeconds(1))
+            .take(10)
+            .map(i -> SimpleRequest.newBuilder().setRequestMessage("hi" + i).build());
+        Flux<SimpleResponse> response = client.streamingRequestAndResponse(request)
+            .log()
+            .doFinally(x -> latch.countDown());
         response.subscribe();
         latch.await();
     }
